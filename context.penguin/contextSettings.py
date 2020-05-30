47b97250ba72565f86926444ed8d0f290d9f0a98
newf276 <ian@kellys.me>
2020-05-29 18:05:12 -0700

    Fixed directory Structure
    Version 2.0

--- context.penguin/contextSettings.py
+++
@@ -1,8 +0,0 @@
-import xbmc
-
-
-if __name__ == '__main__':
-	plugin = 'plugin://plugin.video.penguin/'
-	path = 'RunPlugin(%s?action=contextpenguinSettings&opensettings=false)' % plugin
-	xbmc.executebuiltin(path)
-	xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin)
